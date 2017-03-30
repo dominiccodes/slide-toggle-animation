65f0c23b9e15608627475a5c393b0f01e150f3de
domcodes <email@domcodes.com>
2017-03-30 15:36:04 +0100

    Add JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 (function() {
+	var quoteButton = $('.quote-button');
+			blockquote = $('blockquote');
 	
+	quoteButton.on('click', function(e) {
+			e.preventDefault();
+			var quoteButtonText = quoteButton.text();
+		
+			blockquote.sliceToggle(200, function() {
+				quoteButtonText == "Show more" ? quoteButton.text("Show less") : quoteButton.text("Show more");
+			});
+	});
 	
 })();
